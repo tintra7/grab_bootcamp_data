169cb9f6a497a7f38a3600cf9c094b701de826b0
Tin Tra <tintra17@gmail.com>
2024-05-20 20:09:51 +0700

    add: simulate producer

---
+++ simulate.py
@@ -0,0 +1,27 @@
+import random
+from kafka import KafkaProducer
+import time
+import json
+from time import sleep
+
+sensor_id = "6642237e64e4a690113f5f97"
+
+def serializer(message):
+    return json.dumps(message).encode('utf-8')
+
+producer = KafkaProducer(
+    bootstrap_servers=['localhost:9092'],
+    value_serializer=serializer
+)
+
+while True:
+    
+    msg = {
+        "humidity": random.randint(30, 60),
+        "temp": random.randint(30, 37),
+        "sensor_id": sensor_id,
+        "timestamp": int(time.time())
+    }
+    print("Send", str(msg))
+    producer.send(sensor_id, msg)
+    sleep(1)
